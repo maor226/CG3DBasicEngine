178b43bbba7e63e00ee06552e10b4404ecfc5a85
shtoyer <shtoyer@post.bgu.ac.il>
2022-07-30 18:27:55 +0300

    finished bez move

--- igl/opengl/glfw/imgui/ImGuiMenu.cpp
+++ igl/opengl/glfw/imgui/ImGuiMenu.cpp
@@ -304,7 +304,7 @@ IGL_INLINE void ImGuiMenu::draw_viewer_menu(igl::opengl::glfw::Viewer *viewer, s
     }
 
   }
-  if (ImGui::CollapsingHeader("Tranperent", ImGuiTreeNodeFlags_None))
+  if (ImGui::CollapsingHeader("Transparent", ImGuiTreeNodeFlags_None))
   {
     float w = ImGui::GetContentRegionAvailWidth();
     float p = ImGui::GetStyle().FramePadding.x;
@@ -314,7 +314,7 @@ IGL_INLINE void ImGuiMenu::draw_viewer_menu(igl::opengl::glfw::Viewer *viewer, s
         viewer->data_list[viewer->shapes[viewer->single_picked_shape_idx].shapeIdx]->alpha = viewer->alpha;
       }
     }
-    if(ImGui::SliderFloat("alpha ##Tranperent", &viewer->alpha ,0.f, 1.f)){
+    if(ImGui::SliderFloat("alpha ##Transparent", &viewer->alpha ,0.f, 1.f)){
        if(viewer->single_picked_shape_idx!=-1){
         viewer->data_list[viewer->shapes[viewer->single_picked_shape_idx].shapeIdx]->alpha = viewer->alpha;
       }
@@ -411,87 +411,19 @@ IGL_INLINE void ImGuiMenu::draw_viewer_menu(igl::opengl::glfw::Viewer *viewer, s
     }
   }
 
-  // Viewing options
-  if (ImGui::CollapsingHeader("Viewing Options", ImGuiTreeNodeFlags_None))
-  {
-    if (ImGui::Button("Center object", ImVec2(-1, 0)))
-    {
-      std::cout << "not implemented yet" << std::endl;
-//      core[1].align_camera_center(viewer->data().V, viewer->data().F); TODO: add function like this to camera
-    }
-    //if (ImGui::Button("Snap canonical view", ImVec2(-1, 0)))
-    //{
-    //  core[1].snap_to_canonical_quaternion();
-    //}
-
-    // Zoom
-    ImGui::PushItemWidth(80 * menu_scaling());
-    if (camera[0]->_ortho)
-      ImGui::DragFloat("Zoom", &(camera[0]->_length), 0.05f, 0.1f, 20.0f);
-    else
-      ImGui::DragFloat("Fov", &(camera[0]->_fov), 0.05f, 30.0f, 90.0f);
-
-      // Select rotation type
-    static Eigen::Quaternionf trackball_angle = Eigen::Quaternionf::Identity();
-    static bool orthographic = true;
-
-    // Orthographic view
-    ImGui::Checkbox("Orthographic view", &(camera[0]->_ortho));
-    if (camera[0]->_ortho) {
-        camera[0]->SetProjection(0,camera[0]->_relationWH);
-      }
-    else {
-        camera[0]->SetProjection(camera[0]->_fov > 0 ? camera[0]->_fov : 45,camera[0]->_relationWH);
-      }
-
-      ImGui::PopItemWidth();
-  }
-
-      // Helper for setting viewport specific mesh options
-      auto make_checkbox = [&](const char *label, unsigned int &option)
-      {
-        return ImGui::Checkbox(label,
-          [&]() { return drawInfos[1]->is_set(option); },
-          [&](bool value) { return drawInfos[1]->set(option, value); }
-        );
-      };
-      ImGui::ColorEdit4("Background", drawInfos[1]->Clear_RGBA.data(),
-      ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_PickerHueWheel);
-
-  // Draw options
-  if (ImGui::CollapsingHeader("Draw Options", ImGuiTreeNodeFlags_None))
-  {
-    if (ImGui::Checkbox("Face-based", &(viewer->data()->face_based)))
-    {
-      viewer->data()->dirty = MeshGL::DIRTY_ALL;
-    }
-//
-//    make_checkbox("Show texture", viewer->data().show_texture);
-//    if (ImGui::Checkbox("Invert normals", &(viewer->data().invert_normals)))
-//    {
-//      viewer->data().dirty |= igl::opengl::MeshGL::DIRTY_NORMAL;
-//    }
-    make_checkbox("Show overlay", viewer->data()->show_overlay);
-    make_checkbox("Show overlay depth", viewer->data()->show_overlay_depth);
-
-    ImGui::ColorEdit4("Line color", viewer->data()->line_color.data(),
-        ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_PickerHueWheel);
-    ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.3f);
-    ImGui::DragFloat("Shininess", &(viewer->data()->shininess), 0.05f, 0.0f, 100.0f);
-    ImGui::PopItemWidth();
-  }
-
-  // Overlays
-  if (ImGui::CollapsingHeader("Overlays", ImGuiTreeNodeFlags_None))
-  {
-    make_checkbox("Wireframe", viewer->data()->show_lines);
-    make_checkbox("Fill", viewer->data()->show_faces);
-
-  }
   float w = ImGui::GetContentRegionAvailWidth();
   float p = ImGui::GetStyle().FramePadding.x;
-  if (ImGui::Button("Add Bezier Shape", ImVec2((w-p), 0))){
-          viewer->AddBezierShape();
+    if(ImGui::CollapsingHeader("Bezier", ImGuiTreeNodeFlags_None)) {
+    if (ImGui::Button("Add Shape", ImVec2((w-p), 0))){
+            viewer->AddBezierShape();
+    }
+    if (ImGui::Checkbox("move camera", &viewer->move_camera)) {
+      if(viewer->isActive)
+        viewer->move_camera = !viewer->move_camera;
+    }
+    if (ImGui::Button("update camera", ImVec2((w-p), 0))){
+            viewer->update_camera_bezier();
+    }
   }
   ImGui::End();
 }
